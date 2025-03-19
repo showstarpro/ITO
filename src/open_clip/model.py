bef9e205690d6702755dee73088839f2caceaf23
liuhanpeng <liuhanpeng@lx.com>
2025-03-19 01:49:41 +0000

    update config

--- src/open_clip/model.py
+++ src/open_clip/model.py
@@ -61,55 +61,45 @@ class CLIPVisionCfg:
 
     ls_init_value: Optional[float] = None  # layer scale initial value
     patch_dropout: float = 0.  # what fraction of patches to dropout during training (0 would mean disabled and no patches dropped) - 0.5 to 0.75 recommended in the paper for optimal results
-    attentional_pool: bool = False  # whether to use attentional pooler in the last embedding layer (overrides pool_type)
-    attn_pooler_queries: int = 256  # n_queries for attentional pooler
+    input_patchnorm: bool = False  # whether to use dual patchnorm - would only apply the input layernorm on each patch, as post-layernorm already exist in original clip vit design
+    global_average_pool: bool = False  # whether to global average pool the last embedding layer, instead of using CLS token (https://arxiv.org/abs/2205.01580)
+    attentional_pool: bool = False  # whether to use attentional pooler in the last embedding layer
+    n_queries: int = 256  # n_queries for attentional pooler
     attn_pooler_heads: int = 8  # n heads for attentional_pooling
-    no_ln_pre: bool = False  # disable pre transformer LayerNorm
-    pos_embed_type: str = 'learnable'
-    final_ln_after_pool: bool = False  # apply final LayerNorm after pooling
-    pool_type: str = 'tok'
     output_tokens: bool = False
-    act_kwargs: Optional[dict] = None
-    norm_kwargs: Optional[dict] = None
+    # attn_pooler_queries: int = 256  # n_queries for attentional pooler
+    # no_ln_pre: bool = False  # disable pre transformer LayerNorm
+    # pos_embed_type: str = 'learnable'
+    # final_ln_after_pool: bool = False  # apply final LayerNorm after pooling
+    # pool_type: str = 'tok'
+    # act_kwargs: Optional[dict] = None
+    # norm_kwargs: Optional[dict] = None
 
-    timm_model_name: Optional[str] = None  # a valid model name overrides layers, width, patch_size
-    timm_model_pretrained: bool = False  # use (imagenet) pretrained weights for named model
-    timm_pool: str = 'avg'  # feature pooling for timm model ('abs_attn', 'rot_attn', 'avg', '')
-    timm_proj: str = 'linear'  # linear projection for timm model output ('linear', 'mlp', '')
-    timm_proj_bias: bool = False  # enable bias final projection
-    timm_drop: float = 0.  # head dropout
-    timm_drop_path: Optional[float] = None  # backbone stochastic depth
+    # timm_model_name: Optional[str] = None  # a valid model name overrides layers, width, patch_size
+    # timm_model_pretrained: bool = False  # use (imagenet) pretrained weights for named model
+    # timm_pool: str = 'avg'  # feature pooling for timm model ('abs_attn', 'rot_attn', 'avg', '')
+    # timm_proj: str = 'linear'  # linear projection for timm model output ('linear', 'mlp', '')
+    # timm_proj_bias: bool = False  # enable bias final projection
+    # timm_drop: float = 0.  # head dropout
+    # timm_drop_path: Optional[float] = None  # backbone stochastic depth
 
 
 @dataclass
 class CLIPTextCfg:
-    context_length: int = 77
+context_length: int = 77
     vocab_size: int = 49408
-    hf_tokenizer_name: Optional[str] = None
-    tokenizer_kwargs: Optional[dict] = None
-
     width: int = 512
     heads: int = 8
     layers: int = 12
-    mlp_ratio: float = 4.0
     ls_init_value: Optional[float] = None  # layer scale initial value
+    hf_model_name: str = None
+    hf_tokenizer_name: str = None
+    hf_model_pretrained: bool = True
+    proj: str = 'mlp'
+    pooler_type: str = 'mean_pooler'
     embed_cls: bool = False
     pad_id: int = 0
-    no_causal_mask: bool = False  # disable causal masking
-    final_ln_after_pool: bool = False  # apply final LayerNorm after pooling
-    pool_type: str = 'argmax'
-    proj_bias: bool = False
-    proj_type: str = 'linear'  # control final text projection, 'none' forces no projection
     output_tokens: bool = False
-    act_kwargs: dict = None
-    norm_kwargs: dict = None
-
-    # HuggingFace specific text tower config
-    hf_model_name: Optional[str] = None
-    hf_model_pretrained: bool = True
-    hf_proj_type: str = 'mlp'
-    hf_pooler_type: str = 'mean_pooler'  # attentional pooling for HF models
-
 
 def get_cast_dtype(precision: str):
     cast_dtype = None
