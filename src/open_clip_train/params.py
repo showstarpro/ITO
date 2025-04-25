193b0ccfd39b24c4d65838484820fec1f440e52a
showstarpro <1359947339@qq.com>
2025-04-25 00:30:51 +0000

    update log_dir

--- src/open_clip_train/params.py
+++ src/open_clip_train/params.py
@@ -110,7 +110,7 @@ def parse_args(args):
     parser.add_argument(
         "--logs",
         type=str,
-        default="/lpai/volumes/jfs-data-lhp-bd-ga/ito_mulitpos",
+        default="/lpai/output/models",
         help="Where to store tensorboard logs. Use None to avoid storing logs.",
     )
     parser.add_argument(
